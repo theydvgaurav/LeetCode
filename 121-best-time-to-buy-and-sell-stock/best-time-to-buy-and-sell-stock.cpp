f896b5cbd46fdae7b182e15fd12816aca965f6dd
Gaurav Yadav <54793655+theydvgaurav@users.noreply.github.com>
2025-04-15 23:57:38 +0530

    Time: 51 ms (5.00%) | Memory: 199.4 MB (5.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,33 +5,23 @@ public:
     Solution(){
         dp = vector<int>(100001,-1);
     }
-    int fn(vector<int>& prices,int i){
-        if ( i >= prices.size()) return 0;
-        int res = 0;
-        for(int k=i+1; k < prices.size(); k++){
-            res = max(prices[k] - prices[i],res);
-        }
-        return res;
-    }
-    int solve(vector<int>& prices,int i, vector<int>& min_till_now){
+    int solve(vector<int>& prices,int i, int min_so_far){
         if ( i >= prices.size()) return 0;
         if (dp[i]!=-1) return dp[i];
-        return dp[i]=max(max(prices[i] - min_till_now[i],0),solve(prices,i+1,min_till_now));
+        int profit_if_sell_today = max(prices[i] - min_so_far,0);
+        min_so_far = min(min_so_far,prices[i]);
+        return dp[i]=max(profit_if_sell_today,solve(prices,i+1,min_so_far));
     }
-    void populate_min_till_now(vector<int> &prices, vector<int> &min_till_now){
-        int m_till_now = prices[0];
-        for(auto it: prices){
-            m_till_now = min(m_till_now,it);
-            min_till_now.push_back(m_till_now);
-        } 
-    }
+    // void populate_min_till_now(vector<int> &prices, vector<int> &min_till_now){
+    //     int m_till_now = prices[0];
+    //     for(auto it: prices){
+    //         m_till_now = min(m_till_now,it);
+    //         min_till_now.push_back(m_till_now);
+    //     } 
+    // }
     int maxProfit(vector<int>& prices) {
-        min_till_now.clear();
-        populate_min_till_now(prices,min_till_now);
-        // for(auto it: min_till_now){
-        //     cout << it << " ";
-        // }
-        // cout << "\n";
-        return solve(prices,0,min_till_now);
+        // min_till_now.clear();
+        // populate_min_till_now(prices,min_till_now);
+        return solve(prices,0,prices[0]);
     }
 };
