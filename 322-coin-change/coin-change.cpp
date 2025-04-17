269b2fbcd518dc541453bfbb0656c6c69b2671bf
Gaurav Yadav <54793655+theydvgaurav@users.noreply.github.com>
2025-04-17 19:08:08 +0530

    Time: 22 ms (70.05%) | Memory: 18 MB (69.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    vector<long long int> dp;
-    Solution(){
-        dp = vector<long long int>(10001,-1);
-    }
-    long long int solve(vector<int> coins, long long tgt, long long int curr_sum){
-        if (curr_sum > tgt) return 1e9;
-        if (curr_sum==tgt) return 0;
-        if (dp[curr_sum]!=-1) return dp[curr_sum];
-        long long int temp = 1e9;
-        for(auto it: coins){
-            temp = min(temp, 1 + solve(coins,tgt,curr_sum+it));
-        }
-        return dp[curr_sum] = temp;
-        
-    }
     int coinChange(vector<int>& coins, int amount) {
-        long long int res = solve(coins, amount, 0);
-        return res>=1e9?-1:res;
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (auto coin : coins) {
+                if (i - coin >= 0)
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        return dp[amount]==1e9?-1:dp[amount];
     }
 };
