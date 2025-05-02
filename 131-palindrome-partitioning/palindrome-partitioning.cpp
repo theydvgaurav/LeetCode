b10aef14e11a04abefa30382c7788c2d06666543
Gaurav Yadav <54793655+theydvgaurav@users.noreply.github.com>
2025-05-02 13:07:54 +0530

    Time: 150 ms (10.75%) | Memory: 149 MB (10.47%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -10,26 +10,25 @@ public:
         }
         return true;
     }
-    void solve(string s, int idx, vector<vector<string>>& res,
+    void backtrack(string s, int idx, vector<vector<string>>& res,
                vector<string> ds) {
         if (idx >= s.size()) {
             res.push_back(ds);
             return;
         }
         for (int i = idx; i < s.size(); i++) {
+            // only consider the substrings which are palindrome in itself, and then use the substrings after that. 
             if (isPalindrome(s, idx, i)) {
                 ds.push_back(s.substr(idx, i - idx + 1));
-                solve(s, i + 1, res, ds);
+                backtrack(s, i + 1, res, ds);
                 ds.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
-        res.clear();
-        vector<string> ds;
-        ds.clear();
-        solve(s, 0, res, ds);
+        vector<string> path;
+        backtrack(s, 0, res, path);
         return res;
     }
 };
